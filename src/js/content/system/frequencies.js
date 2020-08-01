c4ab248dbfbc20d6637ce84b04d6a7f7f1343ce3
nicross <nicross@users.noreply.github.com>
2020-08-01 15:44:40 -0500

    Outline frequencies system

--- src/js/content/system/frequencies.js
+++ src/js/content/system/frequencies.js
@@ -2,24 +2,44 @@
 
 content.system.frequencies = (() => {
   // TODO: Perlins
+  // TODO: Root
+
+  function getChord(z) {
+    // TODO: Query perlin: chord type, chord inversion, chord octave
+    return [
+      110,
+      165,
+    ]
+  }
+
+  function getColor(z) {
+    // TODO: Query perlin
+    return 6
+  }
+
+  function toHarmonicSeries(f, color) {
+    const fs = []
+
+    for (let i = 1; i <= color; i += 1) {
+      fs.push(f * i)
+    }
+
+    return fs
+  }
 
   return {
     get: function (z) {
-      // TODO: Resolve frequencies from perlins at z
+      const chord = getChord(z),
+        color = getColor(z)
 
-      return [
-        110,
-        165,
-        220,
-        330,
-        440,
-        660,
-      ]
+      return chord.reduce((fs, f) => [...fs, ...toHarmonicSeries(f, color)], [])
     },
-    reset: function () {
+    import: function () {
       // TODO: Reset perlins
+      // TODO: Derive root from key
+      return this
     },
   }
 })()
 
-engine.state.on('reset', () => content.system.frequencies.reset())
+engine.state.on('import', () => content.system.frequencies.import())
