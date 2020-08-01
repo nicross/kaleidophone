d1acc8fcec3824b3f99b6643885d212670cf97ea
nicross <nicross@users.noreply.github.com>
2020-08-01 14:57:35 -0500

    Disable reverb send

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   app.activate()
   engine.loop.start()
+
+  engine.const.reverbActive = false
+  engine.audio.mixer.auxiliary.reverb.refreshActive()
 })
 
 // TODO: Call engine.audio.start() on first user gesture
